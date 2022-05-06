4546e8260c712633e78b6969b963a12bf9a43782
matheuspython <ruivorobado@gmail.com>
2022-05-06 15:45:22 -0300

    componente de widget completo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
-
-interface ButtonProps{
-  text?: string
-}
-
-const Button = (props: ButtonProps) =>{
-  return (
-    <button className='bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors'>{props.text ?? 'Default'}</button>
-  )  
-}
-
+import { Widget } from './components/Widget'
+ 
 export const App:React.FC = ()=> {
   return (
-    <div className='flex gap-2'>
-     <Button text="Enviar"/>
-     <Button text="Ok"/>
-     <Button />
+    <div>
+      <Widget />
    </div>
   )
 }
