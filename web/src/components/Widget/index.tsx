f1f4c1a9fe019617de42ca3c553933cf26bc6104
matheuspython <ruivorobado@gmail.com>
2022-05-06 16:26:11 -0300

    adcionando acessibilidade ao componente

--- web/src/components/Widget/index.tsx
+++ web/src/components/Widget/index.tsx
@@ -1,16 +1,15 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
 
 export const Widget: React.FC = () => {
-  const [isWidgetOpen,setIsWidgetOpen] = useState(false)
-
-  const toggleWidgetVisibility = ()=> setIsWidgetOpen(!isWidgetOpen)
+  
   
   return (
-    <div className='absolute bottom-5 right-5'>
-      { isWidgetOpen && <p>hello word</p> }
+    <Popover className='absolute bottom-5 right-5'>
+       <Popover.Panel>hello word</Popover.Panel>
      
-      <button onClick={toggleWidgetVisibility} className='  bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+      <Popover.Button  className=' bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
         <ChatTeardropDots className="w-6 h-6" />
    
       <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
@@ -18,7 +17,7 @@ export const Widget: React.FC = () => {
          Feedback
         </span>
       </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }
