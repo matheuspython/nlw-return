27e7270519d9129c3d4d01700418afb6c0f9ae2b
matheuspython <ruivorobado@gmail.com>
2022-05-06 17:15:47 -0300

    iniciando componente com botão de fechar

---
+++ web/src/components/CloseButton/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export const CloseButton: React.FC = () => {
+  return <div />;
+}
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -4,7 +4,15 @@ import React from 'react';
 export const WidgetForm: React.FC = () => {
   return (
     <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
-      hello, word!
+      <header>
+        <span className='text-xl leading-6'>deixe o seu feedback</span>
+      </header>
+
+      <p>teste</p>
+
+      <footer className='text-xs text-neutral-400'>
+        Feito com ❤ por <a className='underline underline-offset-2' href="https://github.com/matheuspython/">Matheus</a>
+      </footer>
     </div>
   )
 }
