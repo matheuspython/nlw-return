fd6cd47208924691d628e2a30a8d05da3667b80e
matheuspython <ruivorobado@gmail.com>
2022-05-06 20:52:59 -0300

    adicionando funcionalidade do feedback

--- web/src/components/CloseButton/index.tsx
+++ web/src/components/CloseButton/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import { Popover } from '@headlessui/react';
+import { X } from 'phosphor-react';
 
 export const CloseButton: React.FC = () => {
-  return <div />;
+  return (
+    <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title='fechar formulario de feedback'>
+      <X weight="bold" className="w-4 h-4" />
+    </Popover.Button>
+  )
 }
